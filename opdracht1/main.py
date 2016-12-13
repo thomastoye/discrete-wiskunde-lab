eaa19ff3cd86d36bae537163290ec648b4c348df
Thomas Toye <toye.thomas@gmail.com>
2016-12-13 11:16:04 +0100

    Laatste aanpassingen aan opdracht 1 voor het verslag

--- opdracht1/main.py
+++ opdracht1/main.py
@@ -4,111 +4,105 @@ from pprint import pprint
 import sys
 from tabulate import tabulate
 
+sys.setrecursionlimit(1500)
+
 """
 Gegeven een restklassenring Zn,+,*. Bepaal de deelgroepen <x> voor alle elementen x e Zxn en controleer de stelling van Lagrange.
 """
 
-n = 7
+n = 126
 
-if(len(sys.argv) > 1):
-  n = int(sys.argv[1])
+# Dit laat toe om een argument mee te geven bij het uitvoeren, bv. `./main.py 13`
+if (len(sys.argv) > 1):
+    n = int(sys.argv[1])
 
+# De klassen Restklassenring
 class Restklassenring:
 
-  def __init__(self, n):
-    self.n = n
-    print("Restklassenring Z%s,+,* gemaakt" % n)
+    # Constructor
+    def __init__(self, n):
+        self.n = n
+        print("Restklassenring Z%s,+,* gemaakt" % n)
 
-  def cayley_tabel_maal(self):
-    return self.cayley_tabel(lambda x, y: x * y)
+    # Methode om een Cayleytabel voor * op te stellen
+    def cayley_tabel_maal(self):
+        return self.cayley_tabel(lambda x, y: x * y)
 
-  def cayley_tabel_plus(self):
-    return self.cayley_tabel(lambda x, y: x + y)
+    # Methode om een Cayleytabel voor + op te stellen
+    def cayley_tabel_plus(self):
+        return self.cayley_tabel(lambda x, y: x + y)
 
-  def cayley_tabel(self, bewerking):
-    header = [self._make_overline(x) for x in range(0, self.n)]
+    # Algemene methode om een Cayleytabel op te stellen. bewerking is een functie (lambda)
+    def cayley_tabel(self, bewerking):
+        header = [self._make_overline(x) for x in range(0, self.n)]
 
-    tabel = [ [ self._make_overline(bewerking(row, col) % self.n) for col in range(0, self.n) ] for row in range(0, self.n) ]
+        tabel = [[self._make_overline(bewerking(row, col) % self.n) for col in range(0, self.n)] for row in
+                 range(0, self.n)]
 
-    return tabel
+        return tabel
 
-  def headers(self):
-    header = [self._make_overline(x) for x in range(0, self.n)]
-    return header
+    # Functie die de headers voor de Cayleytabel maakt
+    def headers(self):
+        header = [self._make_overline(x) for x in range(0, self.n)]
+        return header
 
-  def eenheden(self):
-    # waar je 0 uitkomt bij *
-   tabel = zip(range(0, self.n), self.cayley_tabel_maal())
+    # Berekent de eenheden van de restklassenring (retourneert een arary)
+    def eenheden(self):
+        # waar je 1 uitkomt bij *
+        tabel = zip(range(0, self.n), self.cayley_tabel_maal())
 
-   return [x[0] for x in tabel if self._make_overline(1) in x[1]]
+        return [x[0] for x in tabel if self._make_overline(1) in x[1]]
 
-  def orde(self):
-    return len(self.eenheden())
+    # Berekent de orde van de restklassenring
+    def orde(self):
+        return len(self.eenheden())
 
-  def deelgroepen_Zn_plus(self):
-    return self.deelgroepen(self.cayley_tabel_maal())
+    # Berekent de deelgroepen
+    def deelgroepen_Zxn_maal(self):
+        # oplopende machten
+        inhoud = self.eenheden()
 
-  def deelgroepen_Zxn_maal(self):
-    # oplopende machten
-    inhoud = self.eenheden()
+        inhoud_mapped = {}
 
-    lookup = { x: {} for x in inhoud } # will be 2D, e.g. lookup[1][3]
+        def bepaal_distincte_machten_eenheden(eenheden, distinct, current_value):
+            """<x> = { x^0, x^1, ..., x^n }"""
+            new_value = (x * current_value) % self.n
 
-    for x in inhoud:
-      for y in inhoud:
-        lookup[x][y] = (x * y) % self.n
+            if(new_value in distinct):
+                return distinct
 
-    inhoud_mapped = {}
+            distinct.add(new_value)
 
-    for x in inhoud:
-      start = lookup[x][x]
-      distinct = {start}
-      current_value = start
-
-      while(True):
-        new_value = lookup[x][current_value]
-
-        if(new_value in distinct):
-          break
-
-        distinct.add(new_value)
-        current_value = new_value
-
-      inhoud_mapped[x] = len(distinct)
-
-    return inhoud_mapped
-
-  def deelgroepen(self, tabel):
-    result = {}
-
-    for el in zip(range(0, self.n), tabel):
-      result[el[0]] = len(set(el[1]))
-
-    return result
-
-  def totient(self, n):
-      """
-      totient(n) = aantal getallen 0 < x < n met ggd(x, n) = 1 = orde eenhedengroep Zxn
-      """
+            return bepaal_distincte_machten_eenheden(eenheden, distinct, new_value) # recursieve definitie
 
 
-      pass
+        for x in inhoud:
+            start = (x * x) % self.n
+            distinct = bepaal_distincte_machten_eenheden(inhoud, {start}, start)
 
-  def generatoren(self):
-    result = []
+            inhoud_mapped[x] = len(distinct)
 
-    d = self.deelgroepen_Zxn_maal()
-    for key in d:
-      if(d[key] == self.n - 1):
-        result.append(key)
+        return inhoud_mapped
 
-    return result
+    # Berekent de generatoren
+    def generatoren(self):
+        result = []
 
-  def _make_overline(self, x):
-    return x
-    #return ''.join([char + '\u0305' for char in str(x)])
+        d = self.deelgroepen_Zxn_maal()
+        for key in d:
+            if (d[key] == self.n - 1):
+                result.append(key)
+
+        return result
+
+    # Plaats een streepje boven getallen (met Unicode) voor de duidelijkheid
+    def _make_overline(self, x):
+        return ''.join([char + '\u0305' for char in str(x)])
+
 
 r = Restklassenring(n)
+orde = r.orde()
+
 print('Cayleytabel voor +')
 print(tabulate(r.cayley_tabel_plus(), r.headers(), showindex=True))
 print('\n')
@@ -124,21 +118,22 @@ print('\n')
 print('Orde: ' + str(r.orde()))
 print('\n')
 
-print('Deelgroepen voortgebracht door x\u0305 element van Z%s,+' % n)
-deelgroepen = r.deelgroepen_Zn_plus()
-orde = r.orde()
-for key in deelgroepen:
-  print(' |<' + str(key) + '\u0305>| = ' + str(deelgroepen[key]))
-print('\n')
-
 print('Deelgroepen voortgebracht door x\u0305 element van Zx%s,*' % n)
 deelgroepen = r.deelgroepen_Zxn_maal()
-orde = r.orde()
+lagrange_klopt = True
+
 for key in deelgroepen:
-  print(' |<' + str(key) + '\u0305>| = ' + str(deelgroepen[key]))
+    print(' |<' + str(key) + '\u0305>| = ' + str(deelgroepen[key]) + '\t\t' + str(orde) + ' % ' + str(deelgroepen[key]) + '\t = ' + str(orde % deelgroepen[key]) + ' (Lagrange)')
+
+    if orde % deelgroepen[key] != 0:
+        lagrange_klopt = False
+
+if lagrange_klopt:
+    print('\n ==> De stelling van Lagrange klopt')
+else:
+    print('\n ==> De stelling van Lagrange klopt NIET')
 
 print('\n\n')
 
 print('Generatoren')
 print(r.generatoren())
-
